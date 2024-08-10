67b75a41bdcfd9a5b43fa15001115b89eef56f3e
Jorge Diego daniel Azpeitia <jorgediegodanielazpeitia@MacBook-Pro-de-Jorge.local>
2024-08-10 18:54:29 -0300

    state machine

--- main.go
+++ main.go
@@ -1,1 +1,75 @@
+package golang_machine_state
 
+import "fmt"
+
+type State int
+
+const (
+	Open State = iota
+	InProgress
+	Closed
+)
+
+type Event int
+
+const (
+	StartProgress Event = iota
+	Close
+	Reopen
+)
+
+type Action func()
+
+type StateMachine struct {
+	currentState State
+	transitions  map[State]map[Event]State
+	actions      map[State]map[Event]Action
+}
+
+func NewStateMachine(initialState State) *StateMachine {
+	sm := &StateMachine{
+		currentState: initialState,
+		transitions:  make(map[State]map[Event]State),
+		actions:      make(map[State]map[Event]Action),
+	}
+
+	sm.transitions[Open] = map[Event]State{
+		StartProgress: InProgress,
+	}
+	sm.transitions[InProgress] = map[Event]State{
+		Close: Closed,
+	}
+	sm.transitions[Closed] = map[Event]State{
+		Reopen: Open,
+	}
+
+	sm.actions[Open] = map[Event]Action{
+		StartProgress: func() { fmt.Println("Ticket is now in progress") },
+	}
+	sm.actions[InProgress] = map[Event]Action{
+		Close: func() { fmt.Println("Ticket is now closed") },
+	}
+	sm.actions[Closed] = map[Event]Action{
+		Reopen: func() { fmt.Println("Ticket is reopened") },
+	}
+
+	return sm
+}
+
+func (sm *StateMachine) SendEvent(event Event) {
+	if newState, ok := sm.transitions[sm.currentState][event]; ok {
+		sm.currentState = newState
+		if action, ok := sm.actions[sm.currentState][event]; ok {
+			action()
+		}
+	} else {
+		fmt.Println("Invalid transition")
+	}
+}
+
+func main() {
+	sm := NewStateMachine(Open)
+	sm.SendEvent(StartProgress)
+	sm.SendEvent(Close)
+	sm.SendEvent(Reopen)
+}
